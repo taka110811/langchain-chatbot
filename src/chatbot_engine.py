c2df176512e13b077443cf7021e99e291c50033b
taka110811 <babysteps1108@gmail.com>
2023-09-13 10:27:03 +0900

    indexまで

--- src/chatbot_engine.py
+++ src/chatbot_engine.py
@@ -2,14 +2,20 @@ import langchain
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ChatMessageHistory
 from langchain.schema import HumanMessage
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 
 langchain.verbose = True
 
+def create_index() -> VectorStoreIndexWrapper:
+    loader = DirectoryLoader("./src/", glob="**/*.py")
+    return VectorstoreIndexCreator().from_loaders([loader])
 
-def chat(message: str, history: ChatMessageHistory) -> str:
+
+def chat(
+    message: str, history: ChatMessageHistory, index: VectorStoreIndexWrapper
+) -> str:
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 
-    messages = history.messages
-    messages.append(HumanMessage(content=message))
-
-    return llm(messages).content
+    return index.query(question=message, llm=llm)
